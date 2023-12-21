8b251063120480d0beaabf25a4628addd9e78473
Zimens-Ksenia <zimensksusha@gmail.com>
2023-12-21 15:06:41 +1200

    сделала singleton

--- src/03_Singleton/singleton.py
+++ src/03_Singleton/singleton.py
@@ -1,2 +1,9 @@
+from typing import Optional
 class Singleton:
-    pass
+    single: Optional["Singleton"] = None
+
+    def new(cls, *args, **kwargs):  
+        if not cls.single:
+            cls.single = super().new(cls)
+
+        return cls.single
