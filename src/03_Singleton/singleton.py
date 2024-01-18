ef5597e8518bb0a04eab394933b9e8101eb3e765
Zimens-Ksenia <zimensksusha@gmail.com>
2024-01-18 12:19:42 +1200

    Update singleton.py

--- src/03_Singleton/singleton.py
+++ src/03_Singleton/singleton.py
@@ -1,9 +1,6 @@
-from typing import Optional
 class Singleton:
-    single: Optional["Singleton"] = None
-
-    def new(cls, *args, **kwargs):  
-        if not cls.single:
-            cls.single = super().new(cls)
-
-        return cls.single
+     _instance = None 
+     def __new__(cls): 
+        if cls._instance is None: 
+            cls._instance = super().__new__(cls) 
+        return cls._instance
